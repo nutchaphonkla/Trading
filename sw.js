9dab375be3351ba35a2ea01fb591a0631abaf2b9
nutchaphonkla <nutchaphonsit@gmail.com>
2026-08-08 20:11:44 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,10 @@
-const VERSION='v28-no-cache';
+const VERSION='v29-no-cache';
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
 });
 self.addEventListener('fetch',()=>{});
