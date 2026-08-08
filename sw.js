ec09803d97a39119a2f23570e3be46ba5d9237c1
nutchaphonkla <nutchaphonsit@gmail.com>
2026-08-08 18:12:47 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='onemonth-os-v25-final';
+const CACHE='onemonth-os-v25-1-appmode';
 const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
